9104704d9b130f3107961ffde87437b75725b64b
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-10-18 15:37:56 +0530

    Solved Q5 of Set 1

---
+++ practise_set_1/Q5.java
@@ -0,0 +1,12 @@
+package practise_set_1;
+
+import java.util.Scanner;
+
+public class Q5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter the Value : ");
+        sc.close();
+
+    }
+}

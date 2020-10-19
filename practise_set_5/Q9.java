53d5f5863ad2e93ea3995e2bdc5cd73fa2b15b1e
imyogeshgaur <yogeshgaur1995@gmail.com>
2020-10-19 16:10:32 +0530

    Solved Q9 of set 5

---
+++ practise_set_5/Q9.java
@@ -0,0 +1,12 @@
+package practise_set_5;
+
+public class Q9 {
+    public static void main(String[] args) {
+        int i=1,sum = 0;
+        while (i<11) {
+            sum += 8*i;
+            i++;
+        }
+        System.out.printf("The Sum is : %d",sum);
+    }
+}

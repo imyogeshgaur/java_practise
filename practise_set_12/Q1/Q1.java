6a39dcd3ca56c1b924ad4950079e46592e0eae3b
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-07 10:52:21 +0530

    Solved Q1 of set 12

---
+++ practise_set_12/Q1/Q1.java
@@ -0,0 +1,17 @@
+// package myCalci;
+
+
+// public class SimpleCalculator {
+//     public static void main(String[] args) {
+//         System.out.println("Simple Calculator....!!!!");
+//     }
+// }
+
+// package myCalci;
+
+
+// public class HybridCalculator {
+//     public static void main(String[] args) {
+//         System.out.println("Hybrid Calculator....!!!!");
+//     }
+// }

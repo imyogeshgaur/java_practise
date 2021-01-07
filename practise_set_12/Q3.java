2811cf3481b8bf24118df3843bb354a1c70281b0
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-07 10:53:23 +0530

    Solved Q3 of set 12

--- practise_set_12/Q3.java
+++ practise_set_12/Q3.java
@@ -1,7 +1,7 @@
-// package folder.folderL1.folderL2;
+package folder.folderL1.folderL2;
 
-// public class Q3 {
-//     public static void main(String[] args) {
-//         System.out.println("Level 2 Folder...!!!");
-//     }
-// }
+public class Q3 {
+    public static void main(String[] args) {
+        System.out.println("Level 2 Folder...!!!");
+    }
+}

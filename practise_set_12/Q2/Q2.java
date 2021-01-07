6215cf92c174d05e4458169a2bdd3b83902bf8ab
imyogeshgaur <yogeshgaur1995@gmail.com>
2021-01-07 10:52:31 +0530

    Solved Q2 of set 12

---
+++ practise_set_12/Q2/Q2.java
@@ -0,0 +1,14 @@
+// package Q2;
+
+// import java.util.Scanner;
+
+// public class Q2 {
+//    public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the Number : ");
+//         int num = sc.nextInt();
+//         sc.close();
+//         System.out.println("The number You Entered is : " + num);
+//    }
+    
+// }
